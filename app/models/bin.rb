8901ce41e2c81d307879baac4b017686f052fa21
Jaryd Krishnan <jaryd@harvestprofit.com>
2019-01-30 14:11:44 -0600

    Add methods to Bin and Measurement

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -1,3 +1,11 @@
 class Bin < ApplicationRecord
-  has_many :measurements
+  has_many :measurements, dependent: :destroy
+
+  def radius
+    diameter / 2
+  end
+
+  def radius_squared
+    radius ** 2
+  end
 end
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,15 @@
 class Measurement < ApplicationRecord
   belongs_to :bin
+
+  def square_volume
+    Math::PI * bin.radius_squared * outside
+  end
+
+  def final_in_inches
+    square_volume / 144
+  end
+
+  def final_in_feet
+    final_in_inches / 12
+  end
 end
